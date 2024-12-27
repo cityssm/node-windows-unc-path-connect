6b29f56a00f35810055055fa3e0f9901b2b0825c
Dan Gowans <dangowans@users.noreply.github.com>
2024-12-27 11:38:25 -0500

    linting

--- test/config/config.githubActions.ts
+++ test/config/config.githubActions.ts
@@ -1,7 +1,8 @@
-import { type UncPathOptions } from '../../index.js'
+import type { UncPathOptions } from '../../index.js'
 
 export const validUncPathOptions: UncPathOptions[] = [
   {
+    // eslint-disable-next-line unicorn/prefer-string-raw
     uncPath: '\\\\localhost\\c$'
   }
 ]
